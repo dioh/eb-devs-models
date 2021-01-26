610d27ea22ac1f2dd8a8ba0e586ec8a70faf0cf0
dfoguelman <danilo@mailinator.com>
2021-01-26 12:53:45 -0300

    Initial version

---
+++ sir_informed/experiment.py
@@ -0,0 +1,154 @@
+# Copyright 2014 Modelling, Simulation and Design Lab (MSDL) at 
+# McGill University and the University of Antwerp (http://msdl.cs.mcgill.ca/)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Import code for model simulation:
+from pypdevs.simulator import Simulator
+import itertools
+import os
+import progressbar
+import pandas as pd
+import fileinput
+import tempfile
+import fnmatch
+from matplotlib import pyplot as plt
+# Import the model to be simulated
+from model import Environment, Parameters
+
+
+#    ======================================================================
+
+# 1. Instantiate the (Coupled or Atomic) DEVS at the root of the 
+#  hierarchical model. This effectively instantiates the whole model 
+#  thanks to the recursion in the DEVS model constructors (__init__).
+#
+
+#    ======================================================================
+
+# 2. Link the model to a DEVS Simulator: 
+#  i.e., create an instance of the 'Simulator' class,
+#  using the model as a parameter.
+
+#    ======================================================================
+
+# 3. Perform all necessary configurations, the most commonly used are:
+
+# A. Termination time (or termination condition)
+#    Using a termination condition will execute a provided function at
+#    every simulation step, making it possible to check for certain states
+#    being reached.
+#    It should return True to stop simulation, or Falso to continue.
+# def terminate_whenStateIsReached(clock, model):
+#     return model.trafficLight.state.get() == "manual"
+# sim.setTerminationCondition(terminate_whenStateIsReached)
+
+#    A termination time is prefered over a termination condition,
+#    as it is much simpler to use.
+#    e.g. to simulate until simulation time 400.0 is reached
+
+# B. Set the use of a tracer to show what happened during the simulation run
+#    Both writing to stdout or file is possible:
+#    pass None for stdout, or a filename for writing to that file
+# sim.setVerbose(None)
+
+# C. Use Classic DEVS instead of Parallel DEVS
+#    If your model uses Classic DEVS, this configuration MUST be set as
+#    otherwise errors are guaranteed to happen.
+#    Without this option, events will be remapped and the select function
+#    will never be called.
+
+#    ======================================================================
+
+import model
+import networkx as nx
+from SIRSS_numeric import sir_num
+
+DURATION = 3
+RETRIES = 1
+output_columns = ['t','I','S','R','E', 'retry']
+
+def run_single(retry=0):
+    environ = Environment(name="SIR over CM")
+    sim = Simulator(environ)
+    initial_states = [(ag.state.name, ag.state.state, 0) for ag in environ.agents] 
+    sim.setTerminationTime(DURATION)
+    sim.setClassicDEVS()
+    sim.setDSDEVS(True)
+    topology_name = os.path.basename(Parameters.TOPOLOGY_FILE)
+    sim.setVerbose(None)
+    sim.simulate()
+    dataframe = pd.DataFrame(environ.log_agent.stats)
+    dataframe['retry'] = retry
+    tmpfile = tempfile.NamedTemporaryFile(mode='w', prefix='sir_model', delete=False)
+    dataframe.to_csv(tmpfile, header=False, index=False)
+    outfilename = "results/pa_model_dynamic_graph_%s.gml" % (topology_name)
+    nx.write_gml(environ.G, outfilename)
+
+
+def run_multiple_retries():
+    Parameters.TOPOLOGY_FILE = 'grafos_ejemplo/grafo_chico'
+    # TOPOLOGY_FILE,
+    # N,
+    # INITIAL_PROB,
+    # INFECT_PROB,
+    # BETA_PROB,
+    # RHO_PROB,
+    # RECOV_THRHLD,
+    for i in progressbar.progressbar(range(RETRIES)):
+        run_single(retry=i)
+
+    filenames = [os.path.join("/tmp", f) for f in fnmatch.filter(os.listdir('/tmp'), 'sir_model*')]
+    topology_name = os.path.basename(Parameters.TOPOLOGY_FILE)
+    outfilename = "results/sir_model_%s_emergence_%s_retries_%d.csv" % (topology_name, Parameters.EMERGENT_MODEL, RETRIES)
+    fin = fileinput.input(filenames)
+    with open(outfilename, 'w') as fout:
+        fout.write(",".join(output_columns))
+        fout.write('\n')
+
+        for line in fin:
+            fout.write(line)
+
+    fin.close()
+    for file in filenames: os.remove(file)
+
+    topology_name = os.path.basename(Parameters.TOPOLOGY_FILE)
+
+    data = pd.read_csv(outfilename)
+    filtered_data = data[(data.t > 0)]
+    t,S,I,R=data.t,data.S,data.I,data.R
+
+    
+    fig_filename = outfilename.replace('csv', 'png')
+
+    Sn,In,Rn=sir_num(5000*0.0009,0.0009,0,1,3,8,10000)
+    
+    fig=plt.figure()
+    plt.plot(S,label='S')
+    plt.plot(I,label='I')
+    plt.plot(R,label='R')
+   
+    plt.plot(199*Sn,label='Snumeric')
+    plt.plot(199*In,label='Inumeric')
+    plt.plot(199*Rn,label='Rnumeric')
+    plt.legend()
+    plt.show()
+run_multiple_retries()
+
+#BETA_PROB = 10 RHO_PROB = 0.9
+#T,dt,EK,g,b,lamb,pob
+    
+#    ======================================================================
+
+# 5. (optional) Extract data from the simulated model
+# print("Simulation terminated with traffic light in state %s" % (trafficSystem.trafficLight.state.get()))
---
+++ sir_informed/model.py
@@ -0,0 +1,466 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# TODO: Verificar los elapsed y current time
+
+import sys
+import numpy as np
+import pandas as pd
+import scipy.spatial.distance as distance
+import scipy.spatial as spatial
+from sklearn.neighbors import NearestNeighbors
+import networkx as nx
+from pypdevs.infinity import INFINITY
+
+# Import code for DEVS model representation:
+from pypdevs.DEVS import *
+
+from pypdevs.infinity import INFINITY
+from functools import total_ordering
+
+np.random.seed(1) # con este anda bien
+#np.random.seed(1) # con este rompe
+
+class Parameters:
+    TOPOLOGY_FILE = 'grafos_ejemplo/grafo_vacio'
+    EMERGENT_MODEL = False
+    INITIAL_PROB = 0
+    BETA_PROB = 3
+    RHO_PROB = 1
+    TW_SIZE = 5
+    TW_TRHD = 50
+    TW_BIN_SIZE = 15
+
+DEBUG = True
+
+
+def enum(**kwargs):
+    class Enum(object): pass
+    obj = Enum()
+    obj.__dict__.update(kwargs)
+    return obj
+
+SIRStates = enum(S='Susceptible', I='Infected',  R='Recovered')
+ENVProps = enum(DECAY='decay_rate', AGENT_STATES='log data')
+
+class LogAgent(AtomicDEVS):
+    def __init__(self):
+        self.set_values()
+        self.state = 'pepe'
+        self.stats = []
+        self.name='logAgent'
+        self.current_time = 0 
+        self.elapsed = 0 
+        self.my_input = {}
+
+    def saveLoginfo(self): 
+        parent_items = self.parent.getContextInformation(ENVProps.AGENT_STATES).items()
+        (unique, counts) = np.unique(np.array([it[1][0] for it in parent_items]), return_counts=True) 
+        frequencies = dict(zip(unique, counts))
+        log_data = [frequencies.get(key, 0) for key in SIRStates.__dict__.values()]
+        log_data.insert(0, self.current_time) 
+        self.stats.append(log_data)
+
+    def intTransition(self):
+        self.current_time += self.ta
+        self.saveLoginfo()
+
+    def timeAdvance(self):
+        return self.ta
+
+    def set_values(self):
+        self.ta = 0.1
+
+class AgentState(object):
+    def __init__(self, model, name, id, state):
+        """TODO: to be defined1. """
+        self._name = name
+        self.current_time = 0.0
+
+        self.id = id
+        self._state = state
+        self.vaccinated = False
+        self._to_recover = False
+        self._emergence = False
+        self.neighbors = 0
+        self.free_deg = np.random.poisson(8) if id!=0 else 0
+        self.model = model
+
+        self.neighbors_state = {}
+        self.share = True
+
+        #if self.state == SIRStates.I:
+        #    self.set_infection_values()
+        #else:
+        #if self.state == SIRStates.S:
+        self.ta = INFINITY
+
+    def set_infection_values(self): 
+        #todo: aca falta filtrar por vecinos susceptibles para tirar la moneda
+        prob = 0
+        if self.neighbors>0:
+            #self.neighbors = len(self.model.out_ports_dict)
+            prob = (float(Parameters.RHO_PROB) / (self.neighbors*Parameters.BETA_PROB+Parameters.RHO_PROB))
+            self.to_recover = np.random.random() < prob
+        # self.to_recover = np.random.random() >= Parameters.RHO_PROB
+        #if self.neighbors < 0:
+        #    import pdb;pdb.set_trace()
+            self.ta = np.random.exponential(float(1)/(self.neighbors*Parameters.BETA_PROB+Parameters.RHO_PROB))
+        else:
+            self.ta=np.random.exponential(float(1)/Parameters.RHO_PROB)
+            
+    @property
+    def name(self):
+        """I'm the 'heading' property."""
+        return self._name
+
+    @property
+    def state(self):
+        """I'm the 'heading' property."""
+        return self._state
+
+    @property
+    def to_recover(self):
+        """I'm the 'heading' property."""
+        return self._to_recover
+
+    @to_recover.setter
+    def to_recover(self, to_recover):
+        self._to_recover = to_recover
+
+    @property
+    def emergence(self):
+        """I'm the 'heading' property."""
+        return self._emergence
+
+    @emergence.setter
+    def emergence(self, emergence):
+        self._emergence = emergence
+
+    @state.setter
+    def state(self, state):
+        self._state = state
+
+    @property
+    def ta(self):
+        """I'm the 'heading' property."""
+        return self._ta
+
+    @ta.setter
+    def ta(self, ta):
+        # if ta == 0 and self.current_time > 0:
+        #     __import__('ipdb').set_trace()
+        self._ta = ta
+
+    def get(self):
+        return(self._name, self._state)
+
+    def __repr__(self):
+        return "Agent: %s, State: %s" % (str(self.name), str(self.state))
+
+
+@total_ordering
+class Agent(AtomicDEVS):
+    """
+    A SIR Agent
+    """
+
+    def __init__(self, name=None, id=None):
+        """
+        Constructor (parameterizable).
+        """
+        # Always call parent class' constructor FIRST:
+        AtomicDEVS.__init__(self, name)
+
+        self.in_event = self.addInPort("in_event")
+        self.in_ports_dict ={}
+        self.out_ports_dict= {}
+        # The initial state of the agent.
+        state = SIRStates.I if id==0 else SIRStates.S
+        self.state = AgentState(self, self.name, id, state)
+
+    def extTransition(self, inputs):
+        """
+        External Transition Function.
+        """
+        self.state.current_time += self.elapsed
+
+        # Is it an outreach happening?
+        self.state.share = False
+        print("External")
+        print(self.state)
+        
+        for k, v in inputs.items(): 
+            if v == 'infect':
+                print("Infect")
+                # If an agent is being infected.
+                if self.state.state == SIRStates.S:
+                    self.state.state = SIRStates.I
+                    self.state.share = True
+                # If it is recovered
+                elif self.state.state == SIRStates.R:
+                    print("Ignore, recovered")
+                    self.state.emergence = 0
+                    self.state.ta = INFINITY
+                # If it is vaccinated
+                elif self.state.state == SIRStates.S and (self.state.emergence and Parameters.EMERGENT_MODEL):
+                    print("Ignore, emergence")
+                    self.state.vaccinated = True
+                    self.state.ta = INFINITY
+                # Any other case
+                else:
+                    print("Ignore, just a random msg")
+                    self.state.ta -= self.elapsed
+                self.y_up = self.state
+            else:
+                print("Receiving neighbor update")
+                self.state.neighbors_state[k.name] = v
+                print(str(self.state.neighbors_state))
+                # self.state.ta -= self.elapsed
+        return self.state
+
+    def intTransition(self):
+        """
+        Internal Transition Function.
+        """
+        self.state.current_time += self.timeAdvance()
+        # Emergent behavior is turned off during internal transitions
+        self.state.emergence = 0
+        self.state.share = False
+
+        print("Internal")
+        print(self.state)
+        # If the coin toss resulted in recovery
+        if self.state.to_recover:
+            print("Recovering share = True")
+            self.state.state = SIRStates.R
+            self.state.share = True
+        elif self.state.state == SIRStates.I:
+            print("Re-Infected")
+            self.state.set_infection_values()
+        else:
+            print("Else")
+            self.state.ta = INFINITY
+        self.y_up = self.state
+
+        return self.state
+
+    def __lt__(self, other):
+        if other.state is None:
+            other_name = other.name
+        else:
+            other_name =other.state.name
+        return self.state.name < other_name
+
+    def __hash__(self):
+        return hash(self.state.name)
+
+    def outputFnc(self):
+        print('output')
+        ret = {}
+        if self.state.share:
+            print("Propagating")
+            for outport in self.out_ports_dict.values():
+                ret[outport] = self.state.state 
+            print('ports output: %s ' % str(ret))
+            return ret
+        if self.state.state == SIRStates.I and self.state.to_recover == False:
+            if not self.OPorts:
+                return {}
+            print("Infecting")
+            outport = np.random.choice(self.OPorts)
+            ret = {outport: "infect"}
+            print('ports output: %s ' % str(ret))
+            return ret
+
+        return {}
+
+    def add_connections(self, ag_id): 
+        inport = self.addInPort(name=ag_id)
+        self.in_ports_dict[ag_id] = inport
+        outport = self.addOutPort(name=ag_id)
+        self.out_ports_dict[ag_id] = outport
+        self.state.free_deg -= 1
+        self.state.neighbors +=1
+        return inport, outport
+
+    def modelTransition(self, state):
+        state["newly_infected"] = self.state
+        return self.state.state == SIRStates.I
+
+    def timeAdvance(self):
+        """
+        Time-Advance Function.
+        """
+
+        print('time advance')
+        print(self.state)
+        if self.state.share:
+            print('sharing')
+            self.state.ta = 0
+        elif self.state.state == SIRStates.I:
+
+            print('Infect ta')
+            # self.state.set_infection_values()
+            pass
+        elif self.state.state in (SIRStates.R, SIRStates.S):
+            print('R, S ta')
+            self.state.ta = INFINITY
+
+        # Compute 'ta', the time to the next scheduled internal transition,
+        # based (typically) on current State.
+        return self.state.ta
+
+
+class Environment(CoupledDEVS):
+    def __init__(self, name=None):
+        """
+        A simple flocking system consisting
+        """
+        # Always call parent class' constructor FIRST:
+        CoupledDEVS.__init__(self, name)
+
+        # Children states initialization
+        self.nodes_free_deg = {}
+        self.create_topology()
+        self.G = nx.Graph()
+        self.time_window = {}
+        self.agent_states = {}
+        # Load the agent states dict:
+        for ag in self.agents:
+            self.agent_states[ag.state.name] = (ag.state.state , ag.state.emergence)
+
+        self.stats = []
+
+        self.log_agent = LogAgent()
+        self.log_agent.OPorts = []
+        self.log_agent.IPorts = []
+        self.addSubModel(self.log_agent)
+        self.log_agent.saveLoginfo()
+
+
+    def create_topology(self):
+        G = nx.read_adjlist(Parameters.TOPOLOGY_FILE)
+        self.agents = [Agent(name="agent %d" % i, id=i) for i in range(G.number_of_nodes())]
+        #un agente infectado conectado con algunos vecinos S
+        self.G = nx.read_adjlist(Parameters.TOPOLOGY_FILE)
+
+        for agent in self.agents:
+            self.addSubModel(agent)
+
+        for i in G.edges():
+            if i[0] == i[1]: continue
+            ind0 = int(i[0])
+            ind1 = int(i[1])
+            a1 = self.agents[ind0]
+            a2 = self.agents[ind1]
+            out1 = a1.addOutPort("from%d-to-%d" % (ind0, ind1))
+            out2 = a2.addOutPort("from%d-to-%d" % (ind1, ind0))
+
+            self.connectPorts(out1, a2.in_event)
+            self.connectPorts(out2, a1.in_event)
+        
+        self.nodes_free_deg[0]=0
+        
+        for agent in self.agents:
+            self.nodes_free_deg[agent.state.id]=agent.state.free_deg
+            agent.state.neighbors = len(agent.OPorts)
+            #if agent.state.state == SIRStates.I:
+            #    agent.state.set_infection_values()
+        
+        
+        
+        self.G = nx.relabel.convert_node_labels_to_integers(self.G)
+        self.agents[0].state.state=SIRStates.I
+        self.agents[0].state.set_infection_values()      
+
+    def globalTransition(self, e_g, x_b_micro, *args, **kwargs):
+        super(Environment, self).globalTransition(e_g, x_b_micro, *args, **kwargs)
+        for state in x_b_micro: 
+            #self.nodes_free_deg[state.id] = state.free_deg
+            self.agent_states[state.name] = (state.state, state.emergence)
+
+    def getContextInformation(self, property, *args, **kwargs):
+        super(Environment, self).getContextInformation(property)
+        if(property == ENVProps.AGENT_STATES):
+            return self.agent_states
+
+    def select(self, immChildren):
+        """
+        Choose a model to transition from all possible models.
+        """
+        # Doesn't really matter, as they don't influence each other
+        return immChildren[0]
+
+    def modelTransition(self, state): 
+        # Sort a random value from the weighted list of nodes
+        newly_inf=state["newly_infected"]
+        current_time=newly_inf.current_time
+        newly_inf_id=newly_inf.id        
+        newly_inf_deg = newly_inf.free_deg
+        self.nodes_free_deg[newly_inf.id] = 0
+        #import pdb;pdb.set_trace()        
+        #self.agents[newly_inf_id].state.free_deg = 0 
+        grados = list(self.nodes_free_deg.values())
+        # if any(np.array(self.nodes_free_deg.values()) < 0):
+        #     __import__('ipdb').set_trace()
+        #todo Avisar a los agentes los cambios en sus valores de vecinos
+        
+
+        xk = np.array(grados)
+        xk[newly_inf_id]=0
+        
+        if sum(xk) == 0:
+            return False
+        
+        
+        pk = xk / float(sum(xk))
+        #esto es para el evento SS
+        p=0
+        K=0
+
+        deg=max(0,newly_inf_deg+K*np.random.binomial(1,p))
+        
+        if deg > sum(pk>0):
+            return False
+        
+        if any(pk < 0):
+            import pdb;pdb.set_trace()
+        
+        selected_agents = np.random.choice(max(0,list(self.nodes_free_deg.keys())), deg, p=pk,replace=False)
+        if any([ self.agents[ag].state.free_deg <= 0 for ag in selected_agents]):
+            __import__('ipdb').set_trace()
+
+        
+
+        # Connect ports from/to that node
+        for i in selected_agents:
+            i0, o0 = self.agents[newly_inf_id].add_connections(i)
+            i1, o1 = self.agents[i].add_connections(newly_inf_id)
+
+        #i0, o0 = new_agent.add_connections(selected_agent_id)
+        #i1, o1 = self.agents[selected_agent_id].add_connections(new_ag_id)
+
+            self.connectPorts(o0, i1)
+            self.connectPorts(o1, i0)
+
+            # Update the nodes free degrees list
+            self.nodes_free_deg[i] = self.nodes_free_deg[i]-1
+            self.G.add_edge(int(newly_inf_id), int(i), timestamp=current_time)
+        
+        #self.agents[newly_inf_id].state.set_infection_values()
+        self.agents[newly_inf_id].state.free_deg = 0
+        return False
+
+
